ed3ddcaeef960868ecf48697a3640cd207c7df9f
juanda822 <jdsanchez8522@gmail.com>
2025-09-27 01:23:48 +0000

    menu, detalle, paginas

---
+++ js/Detalle.js
@@ -0,0 +1,21 @@
+async function Detalle(pokemon) {
+
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokemon);
+        const data = await res.json();
+
+        console.log(data.forms[0].name)
+        console.log(data.id)
+
+        document.getElementById("root").innerHTML = `
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png" alt="${data.name}" height="120" width="auto">
+        <p>${data.name}</p>
+        <p>${data.id}</p>
+        <p>Altura: ${data.height / 10} m / Peso: ${data.weight / 10} kg</p>
+        <p>hp: ${data.stats[0].base_stat}</p>
+        <p>Velocidad: ${data.stats[5].base_stat}</p>
+        <p>Ataque: ${data.stats[1].base_stat} Defensa: ${data.stats[2].base_stat}</p>
+        <p>Ataque Especial: ${data.stats[3].base_stat} Defensa Especial: ${data.stats[4].base_stat}</p>`
+}
+
+
+Detalle(160)
